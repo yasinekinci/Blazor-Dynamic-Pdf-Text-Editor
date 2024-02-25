583131b910d47829c15dfda83cdc53c53c4ae15a
Yasin Ekinci <yasin.ekinci@dzdtech.com>
2024-02-25 22:57:43 +0300

    add index page

---
+++ Blazor-Dynamic-Pdf-Text-Editor/Model/Document.cs
@@ -0,0 +1,11 @@
+﻿
+public class Document
+{
+	public byte[] Data { get; set; }
+	public string TextValue { get; set; }
+	public int XCoordinate { get; set; }
+	public int YCoordinate { get; set; }
+	public int PageNumber { get; set; }
+
+}
+
--- Blazor-Dynamic-Pdf-Text-Editor/Program.cs
+++ Blazor-Dynamic-Pdf-Text-Editor/Program.cs
@@ -1,11 +1,12 @@
 using Blazor_Dynamic_Pdf_Text_Editor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
